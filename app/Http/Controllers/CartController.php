5bd5dc19508394e535c73397247db113c6039e62
Win <harrychan2139@gmail.com>
2018-01-21 15:45:07 -0500

    Cart Repair

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -9,16 +9,31 @@ use Cart;
 
 class CartController extends Controller
 {
-    public function add(){
-        if (Request::isMethod('post')) {
-            $product_id = Request::get('id');
-            $product = Product::find($product_id);
-            Cart::add(array('id' => $product_id, 'name' => $product->name, 'qty' => 1, 'price' => $product->price));
+
+
+        public function add()
+        {
+
+            if (Request::isMethod('post')) {
+
+                $product_id = Request::get('id');
+                $product = Product::find($product_id);
+                //return $product->image;
+                Cart::add(
+                    [
+                        'id' => $product_id,
+                        'name' => $product->name,
+                        'image' => $product->image,
+                        'gty' => 1,
+                        'price' => $product->price
+                    ]
+                );
+            }
+            // Cart::add($product);
+
+            $cart = Cart::content();
+            return $cart;
+            // return redirect('/products');
+            // return view('cart', arßray('cart' => $cart, 'title' => 'Welcome', 'description' => '', 'page' => 'home'));
         }
-
-        $cart = Cart::content();
-        return redirect('/products');
-        // return view('cart', arßray('cart' => $cart, 'title' => 'Welcome', 'description' => '', 'page' => 'home'));
-
     }
-}
